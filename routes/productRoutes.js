127a62660a1c523ba260e93fb44107d0d3e116ad
Duru Nef Özmen <duru.ozmen@sabanciuniv.edu>
2025-04-16 23:33:46 +0300

    login register profile product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,14 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const authMiddleware = require('../middleware/authMiddleware'); // Eğer auth middleware ile korumak istiyorsanız
 
+// Get all products
+router.get('/', productController.getProducts);
+
 // Ürün ekleme: authMiddleware kullanıyorsanız yalnızca giriş yapmış kullanıcı eklesin
 router.post('/', authMiddleware, productController.createProduct);
+
+router
+  .route('/:id')
+  .get(productController.getProductById);
 
 module.exports = router;
