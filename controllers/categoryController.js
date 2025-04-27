d9631c020cff804997408239740f169aaa3591ab
Omer Fatih Tarim <omer.tarim@sabanciuniv.edu>
2025-04-28 00:04:47 +0300

    feat: integrate cart & invoice static routes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,29 +1,32 @@
 const Category = require('../models/categoryModel');
-const Product = require('../models/productModel');
 const catchAsync = require('../utils/catchAsync');
 
 // Yeni kategori oluşturma
 exports.createCategory = catchAsync(async (req, res, next) => {
   const newCategory = await Category.create(req.body);
-
   res.status(201).json({
     status: 'success',
-    data: {
-      category: newCategory
-    }
+    data: { category: newCategory }
   });
 });
 
-// Belirli bir kategoriye ait ürünleri listeleme (virtual populate kullanılarak)
+// Tüm kategorileri listeleme
+exports.getAllCategories = catchAsync(async (req, res, next) => {
+  const categories = await Category.find();
+  res.status(200).json({
+    status: 'success',
+    results: categories.length,
+    data: { categories }
+  });
+});
+
+// Belirli bir kategoriye ait ürünleri listeleme
 exports.getProductsByCategory = catchAsync(async (req, res, next) => {
   const { id } = req.params;
-
-  // Kategori bulunduysa, ilgili ürünleri de populate eder
   const category = await Category.findById(id).populate('products');
   if (!category) {
-    return res.status(404).json({ message: 'Category not found' });
+    return res.status(404).json({ status: 'fail', message: 'Category not found' });
   }
-
   res.status(200).json({
     status: 'success',
     data: {
