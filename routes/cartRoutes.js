7fd8edc1d692358daac6bab27948ced7ac90ed57
Duru Nef Özmen <duru.ozmen@sabanciuniv.edu>
2025-04-27 03:45:40 +0300

    login, register, product, sort, productinfo, cart ekleme cıkarma, cartta sıkıntı var ürün updatelerken anası sikiliyo biri buna baksa iyi olur yani 4 tane ekledim artıya basıp bi tane daha ekliceksem sıçılıyo, order page, confirmation, mail gönderiyo mü checkleyemedim, profile da order historyde çıkıyo order status falan, reviewle alakalı hiçbişey yapamadım comment ve review endpointleri de anlamadım

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -7,6 +7,7 @@ const optionalAuth = require('../middleware/optionalAuth');
 // Eğer token varsa doğrulanır, yoksa guest olarak devam eder.
 router.get('/', optionalAuth, cartController.getCart);
 router.post('/add', optionalAuth, cartController.addItemToCart);
+router.post('/update', optionalAuth, cartController.updateCartItem);
 router.post('/remove', optionalAuth, cartController.removeItemFromCart);
 
 module.exports = router;
